bcbdb1ad2cad71a0f761329d5708327c594f383a
Aleksandr KOZAREV <>
2026-02-19 16:21:35 +0100

    fix black

--- datacatalog/solr/solr_orm.py
+++ datacatalog/solr/solr_orm.py
@@ -179,7 +179,7 @@ class SolrQuery(object):
         cursor_mark = None
         if self.cursor_enabled:
             cursor_mark = cursor or "*"
-            if sort_with_order and not re.search(r'\bid (asc|desc)', sort_with_order):
+            if sort_with_order and not re.search(r"\bid (asc|desc)", sort_with_order):
                 sort_with_order += ", id asc"
             else:
                 sort_with_order = "id asc"
